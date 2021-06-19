ff4565c4984d8609d5d13131c51a9a0850b93816
vrex808 <vrex808@email.com>
2021-06-18 22:59:29 -0700

    edit html and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $("#application").submit(function(event) {
-  event.preventDefault()
+  $(".btn").submit(function(event) {
+    event.preventDefault()
     const person = $("input#name").val();
     const gender = $("select#gender").val();
     const dob = $("#born").val();
