8d01f500febeedcbc39355608dcc318d9046da12
vrex808 <vrex808@email.com>
2021-06-19 14:11:34 -0700

    add style to h1, h3 and body color

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".btn").click(function(event) {
-    event.preventDefault()
+    event.preventDefault();
     const person = $("input#name").val();
     const gender = $("select#gender").val();
     const question2 = $("select#yes-question2")
@@ -10,10 +10,16 @@ $(document).ready(function() {
 
     $(".name").text(person);
 
-    if (gender === 'male' && 'female' && question2 && question3 && question6 >= yes);
+    if (gender === 'male' && 'female' && question2 === yes);
+      $("#csharp").show(); $("#app").show();
+    if (gender === 'male' && 'female' && question3 === yes);
       $("#python").show(); $("#app").show();
-      if (gender === 'male' && 'female' && question2 && question3 && question5 && question6 >= no);
+    if (gender === 'male' && 'female' && question6 === yes);
+      $("#html").show(); $("#app").show();
+    if (gender === 'male' && 'female' && question2 && question3 && question5 && question6 === no);
       $("#javascript").show(); $("#app").show();
-  
+    if (gender === 'male' && 'female' && question5 === yes);
+      $("#react").show(); $("#app").show();
+
     });
 });
