cd668fa04328da40bda0af893bca0d026d0fcc3f
vrex808 <vrex808@email.com>
2021-06-18 23:20:23 -0700

    add more functions to js

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,25 @@ $(document).ready(function() {
     event.preventDefault()
     const person = $("input#name").val();
     const gender = $("select#gender").val();
-    const dob = $("#born").val();
+    const question2 = $("select#yes-question2")
+    const question3 = $("select#yes-qeustion3")
+    const question5 = $("select#yes-question5")
+    const question6 = $("#select#yes-question6")
 
     $(".name").text(person);
+    $("#app").show();
     $("#html").show();
     $("#python").show();
     $("#csharp").show();
     $("#javascript").show();
     $("#react").show();
     
-  });
+    if (gender === 'male' || 'female' && question2 < yes);
+    if (gener === 'male' || 'female' && question2 > yes);
+
+    if (gender === 'male' || 'female' && question3  );
+    if (gender === 'male' || 'female' && question5 );
+    if (gender === 'male' || 'female' && question6 );
+    });
 });
 
