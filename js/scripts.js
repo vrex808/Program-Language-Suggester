ec563cfee4837d493e908e7b5a5cf06500822266
vrex808 <vrex808@email.com>
2021-06-20 08:07:18 -0700

    add col to three answers

--- js/scripts.js
+++ js/scripts.js
@@ -3,33 +3,33 @@ $(document).ready(function() {
     event.preventDefault();
     clearDivs();
     const person = $("input#name").val();
-    let question2 = $("select#question2").val();
-    let question3 = $("select#yes-question3").val();
-    let question5 = $("select#yes-quesiton5").val();
-    let question6 = $("select#yes-question6").val();
+    let question2 = $("#question2").val();
+    let question3 = $("#question3").val();
+    let question5 = $("#quesiton5").val();
+    let question6 = $("#question6").val();
 
     $(".name").text(person);
 
-    if (question2 == "no" && question3 == "no" && question5 == "no" && question6 == "no") {
+    if (question2 === "no" && question3 === "no" && question5 === "no" && question6 === "no") {
       $("#javascript").show(); 
       $("#app").show();
 
-    } else if (question2 == "yes" && question3 == "yes" && question5 == "yes" && question6 == "yes") {
+    } else if (question2 === "yes" && question3 === "yes" && question6 === "yes") {
       $("#csharp").show(); 
       $("#app").show();
     }
     
-    else if (question3 == "yes") {
+    else if (question3 === "yes") {
       $("#python").show(); 
       $("#app").show();
     }
 
-    else if (question6 == "yes") {
+    else if (question6 === "yes") {
       $("#html").show(); 
       $("#app").show();
     }
 
-    else if (question5 == "yes"); {
+    else if (question5 === "yes"); {
       $("#react").show(); 
       $("#app").show();
     }
