b5713918eb5f1e10e4beea01b3310357aeda505e
vrex808 <vrex808@email.com>
2021-06-19 22:05:01 -0700

    fix JS

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,35 @@ $(document).ready(function() {
     event.preventDefault();
     clearDivs();
     const person = $("input#name").val();
-    const gender = $("input#gender").val();
     let question2 = $("select#question2").val();
     let question3 = $("select#question3").val();
     let question5 = $("select#quesiton5").val();
     let question6 = $("select#question6").val();
-    let no = $("select#question2, question3, question5, question6").val();
-    
+    const no = question2 && question3 && question5 && question6
 
     $(".name").text(person);
 
-    if (question2 === "select#question2"); 
-      $("#csharp").show(); $("#app").show();
+    if (question2 >= "select#question2") {
+      $("#csharp").show(); 
+      $("#app").show();
     
-    if (question3 === "select#question3");
-      $("#python").show(); $("#app").show();
+    } else if (question2 > "select#question3") {
+      $("#python").show(); 
+      $("#app").show();
+    }
 
-    if (question6 === "select#question6");
-      $("#html").show(); $("#app").show();
+    if (question6 >= "select#question6") {
+      $("#html").show(); 
+      $("#app").show();
 
-    if (question2 && question3 && question5 && question6 === no);
-      $("#javascript").show(); $("#app").show();
+    } else if (question2 && question3 && question5 && question6 <= no) {
+      $("#javascript").show(); 
+      $("#app").show();
+    }
 
-    if (question5 === "select#question5");
-      $("#react").show(); $("#app").show();
+    if (question5 >= "select#question5");
+      $("#react").show(); 
+      $("#app").show();
     
 
     });
